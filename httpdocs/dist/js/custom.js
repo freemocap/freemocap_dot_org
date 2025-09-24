8d5c244cd19173b8078c214e1490ee466ccee3c5
jonmatthis <jonmatthis@gmail.com>
2025-09-24 09:36:24 -0400

    rm console log

--- httpdocs/dist/js/custom.js
+++ httpdocs/dist/js/custom.js
@@ -8,11 +8,8 @@ fetch("footer.html")
   .then(response => response.text())
   .then(data => {
     document.getElementById("footer").innerHTML = data;
-    // Set the year after footer is loaded
-    const year = new Date().getFullYear();
-    console.log(`Year: ${year}`);  // Fixed syntax
     const yearElement = document.getElementById("year");
     if (yearElement) {
-      yearElement.textContent = year;
+      yearElement.textContent = new Date().getFullYear();
     }
   });
